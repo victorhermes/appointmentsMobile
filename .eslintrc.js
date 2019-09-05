03e2e78fb57ad9252b2c2b67da945678e46ca52b
Victor <me.victorhermes@gmail.com>
2019-09-05 20:54:55 -0300

    configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: '@react-native-community',
+    setting: {
+        'import/resolver': {
+            'babel-plugin-root-import': {
+                rootPathSuffix: 'src',
+            },
+        },
+    },
 };
