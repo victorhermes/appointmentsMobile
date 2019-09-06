90434fc5994f909db77598a13f09439284a15183
Victor <me.victorhermes@gmail.com>
2019-09-05 21:06:18 -0300

    configs

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import {Text} from 'react-native';
+import Testo from '~/text';
+
+const App = () => {
+    return (
+        <Fragment>
+            <Testo />
+        </Fragment>
+    );
+};
+
+export default App;
---
+++ src/text.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Testo = () => {
+    return <Text>vasdasdctor</Text>;
+};
+
+export default Testo;
