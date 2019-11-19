3efe25dccbdc6c21cbfdc329411aa845b55a6f46
Victor Hermes <me.victorhermes@gmail.com>
2019-11-19 09:18:49 -0300

    reactotron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -4,7 +4,7 @@ import reactotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
     const tron = Reactotron.configure({
-        host: '192.168.0.109',
+        host: 'localhost',
     })
         .useReactNative()
         .use(reactotronRedux())
